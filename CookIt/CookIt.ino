1b5e3cabd9e690969be9fe0482e43986e2babd69
pas218 <pas218@pitt.edu>
2024-10-23 11:04:20 -0400

    Prototyped ChopIt

--- CookIt/CookIt.ino
+++ CookIt/CookIt.ino
@@ -1,9 +1,46 @@
-void setup() {
-  // put your setup code here, to run once:
+typedef struct
+{
+     int analogInputNumber;
+     float expectVoltage;
+}ingredient;
 
+ingredient Burger  = {0, 1};
+ingredient Lettuce = {0, 1};
+ingredient Cheese  = {0, 1};
+ingredient Onion   = {0, 1};
+
+void setup() {
+  pinMode(0, INPUT_PULLUP);
+  pinMode(1, OUTPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  
+  digitalWrite(1, HIGH);
+  delay(1000);
+  chopIt();
+}
+
+void chopIt() {
+
+  int sensorVal;
+
+  while(true){
+    sensorVal = digitalRead(0);
+
+    if (sensorVal == LOW){
+      digitalWrite(1, LOW);
+      delay(2000);
+      break;
+    }
+  }
+
+}
+
+void cookIt(){
+
+}
+
+void assembleIt(){
 
 }
