2fb8fa8a2b25602d4e10e08d8f033316145ef18d
pas218 <pas218@pitt.edu>
2024-10-24 19:37:29 -0400

    Prototyped Chop-It and Cook-It

--- CookIt/CookIt.ino
+++ CookIt/CookIt.ino
@@ -1,3 +1,18 @@
+// Define Chop-It Inputs;
+#define inputChopIt 5
+
+// Rotary Encoder Inputs
+ #define inputCLK 0
+ #define inputDT 7
+
+  // LED Outputs
+ #define ledCW 8
+ #define ledCCW 9
+
+ int counter = 0; 
+ int currentStateDT;
+ int previousStateDT; 
+
 typedef struct
 {
      int analogInputNumber;
@@ -9,28 +24,59 @@ ingredient Lettuce = {0, 1};
 ingredient Cheese  = {0, 1};
 ingredient Onion   = {0, 1};
 
+long randNumber; 
+
 void setup() {
-  pinMode(0, INPUT_PULLUP);
-  pinMode(1, OUTPUT);
+
+  // Set encoder pins as inputs  
+  pinMode (inputCLK,INPUT);
+  pinMode (inputDT,INPUT);
+
+  // Read the initial state of inputCLK
+  // Assign to previousStateCLK variable
+  previousStateDT = digitalRead(inputDT);
+
+  pinMode(inputChopIt, INPUT_PULLUP);
+
+  randomSeed(analogRead(0));
+
+  // Setup Serial Monitor
+   Serial.begin (9600);
 }
 
 void loop() {
   
-  digitalWrite(1, HIGH);
-  delay(1000);
-  chopIt();
+  delay(2000);
+
+  randNumber = random(0, 2);
+  // Chop it
+  if (randNumber == 0){
+    Serial.print("Chop-It!");
+    Serial.println();
+    chopIt();
+  }
+  // Cook it
+  else{
+    Serial.print("Cook-It!");
+    Serial.println();
+    cookIt();
+  }
 }
 
 void chopIt() {
 
+  // Define the sensor value.
   int sensorVal;
 
   while(true){
-    sensorVal = digitalRead(0);
+    // Read the value of the digital input associated with Chop-It.
+    sensorVal = digitalRead(inputChopIt);
 
+    // If the digital input is low (meaning the knife has been used to complete the circuit),
+    // turn off the LED to signify that the input has been read and break from the infinite loop.
     if (sensorVal == LOW){
-      digitalWrite(1, LOW);
-      delay(2000);
+      Serial.print("Chop-It recieved.");
+      Serial.println();
       break;
     }
   }
@@ -39,6 +85,37 @@ void chopIt() {
 
 void cookIt(){
 
+  
+
+  while(true){
+
+    // Read the current state of inputCLK
+    currentStateDT = digitalRead(inputDT);
+
+    // If the previous and the current state of the inputCLK are different then a pulse has occured
+    if (currentStateDT != previousStateDT){ 
+          
+        // If the inputDT state is same as the inputCLK state then 
+        // the encoder is rotating counterclockwise
+        if (digitalRead(inputCLK) == currentStateDT) { 
+          counter++; 
+        }
+        else {
+          counter--;
+        }
+      } 
+      // Update previousStateCLK with the current state
+      previousStateDT = currentStateDT;
+
+      if(counter > 7){
+          Serial.print("Cook-It recieved.");
+          Serial.println();
+          break;
+      }
+    }
+
+    // Reset the counter for the
+    counter = 0;
 }
 
 void assembleIt(){
